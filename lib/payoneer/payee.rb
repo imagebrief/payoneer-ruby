8cae5d088523e956e6861eb0a77256acc3ee0a30
Harry Curotta <hcurotta@gmail.com>
2016-09-12 17:18:24 -0400

    optional payment type param

--- lib/payoneer/payee.rb
+++ lib/payoneer/payee.rb
@@ -4,17 +4,17 @@ module Payoneer
   class Payee
     SIGNUP_URL_API_METHOD_NAME = 'GetToken'
 
-    def self.signup_url(payee_id, payout_method="IACH", existing_account=false, redirect_url=nil, redirect_time=nil)
+    def self.signup_url(payee_id, payout_method=nil, existing_account=false, redirect_url=nil, redirect_time=nil)
       payoneer_params = {
         p4: payee_id,
         p6: redirect_url,
         p8: redirect_time,
         p9: Payoneer.configuration.auto_approve_sandbox_accounts?,
         p10: true, # returns an xml response
-        p11: payout_method,
       }
 
-      payoneer_params = payoneer_params.merge({p12: "AlreadyHaveAnAccount"}) if existing_account 
+      payoneer_params = payoneer_params.merge({p11: payout_method}) if payout_method
+      payoneer_params = payoneer_params.merge({p12: "AlreadyHaveAnAccount"}) if existing_account  
 
       response = Payoneer.make_api_request(SIGNUP_URL_API_METHOD_NAME, payoneer_params)
 
