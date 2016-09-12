c5091dbbc63feed56c5c148edfb5d9b668b9ce04
Harry Curotta <hcurotta@gmail.com>
2016-09-12 16:00:39 -0400

    added previous account and payment method params

--- lib/payoneer/payee.rb
+++ lib/payoneer/payee.rb
@@ -4,15 +4,18 @@ module Payoneer
   class Payee
     SIGNUP_URL_API_METHOD_NAME = 'GetToken'
 
-    def self.signup_url(payee_id, redirect_url=nil, redirect_time=nil)
+    def self.signup_url(payee_id, payout_method="IACH", existing_account=false, redirect_url=nil, redirect_time=nil)
       payoneer_params = {
         p4: payee_id,
         p6: redirect_url,
         p8: redirect_time,
         p9: Payoneer.configuration.auto_approve_sandbox_accounts?,
         p10: true, # returns an xml response
+        p10: payout_method,
       }
 
+      payoneer_params.merge({p12: "AlreadyHaveAnAccount"}) if existing_account 
+      
       response = Payoneer.make_api_request(SIGNUP_URL_API_METHOD_NAME, payoneer_params)
 
       if success?(response)
